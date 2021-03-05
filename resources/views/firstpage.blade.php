8d9db620c06305080edd3227d584302130687c6c
rajuraj1994 <rajurajkandel@gmail.com>
2021-03-05 13:06:19 +0545

    fourth commit

--- resources/views/firstpage.blade.php
+++ resources/views/firstpage.blade.php
@@ -2,14 +2,18 @@
 <html>
 <head>
 	<title>firstpage</title>
+	<style>
+		h1{
+			color: red;
+			text-shadow: 2px 2px 3px blue;
+		}
+		span{
+			color:orange;
+		}
+	</style>
 </head>
 <body>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<marquee><h1>Ichchya <span>Shrestha	</span></h1></marquee>
 
 </body>
 </html>
